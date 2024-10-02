6ce89fdf5b25b5558a8fdc84b46ead609fb5a607
remyamp <remyamp774@gmail.com>
2024-10-02 09:00:00 +0530

    2-10-24

--- lib/cart.dart
+++ lib/cart.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:meesho/database.dart';
+
 class Cartexample extends StatelessWidget{
   @override
   Widget build(BuildContext context){
@@ -8,87 +8,135 @@ class Cartexample extends StatelessWidget{
         "CART",style: TextStyle(fontSize: 25),
       ),),
       body:
-      ListView.separated(
-       separatorBuilder: (context,index){
-        return  SizedBox(
-                  height:25);
-       },
-        itemCount: 1,
-        itemBuilder: (context,index){
-      return Container(
-        height: 150,
-        width: double.infinity,
-    color: const Color.fromARGB(255, 238, 228, 228),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Container(
-                height: 70,
-                width: 100,
-                decoration: BoxDecoration(
-                  image: DecorationImage(image: AssetImage(
-                    "assets/images/ph2.jpeg"
-                  ),fit: BoxFit.cover)
+       Column(
+         children: [
+           Container(
+            height: 150,
+            width: double.infinity,
+               color: const Color.fromARGB(255, 238, 228, 228),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    height: 70,
+                    width: 100,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(image: AssetImage(
+                        "assets/images/ph2.jpeg"
+                      ),fit: BoxFit.cover)
+                    ),
+                  ),
                 ),
-              ),
+                
+               Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+               children: [
+                 Text("Party Wear Long Cut Kurti",style: TextStyle(fontSize: 15),),
+               ]
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                 Icon(Icons.currency_rupee),
+                 Text("600",style: TextStyle(fontSize: 15),),    Text("  9% off",style: TextStyle(color: const Color.fromARGB(255, 110, 108, 108)),)
+               
+               ],
+             ),
+             Row(
+              children: [
+                Text("All issue easy returns",style: TextStyle(color: const Color.fromARGB(255, 204, 202, 202)),)
+              ],
+             ),
+           
+              Row(
+              children: [
+                Text("Size:32",style: TextStyle(color: const Color.fromARGB(255, 204, 202, 202)),)
+              ],
+             ),
+             SizedBox(height: 10),
+              Row(
+              children: [
+                Icon(Icons.delete),
+                Text("Remove",style: TextStyle(color: const Color.fromARGB(255, 204, 202, 202)),)
+              ],
+             ),
+                ],
+               ),
+           
+              
+               
+                
+              ],
             ),
             
-           Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
-           children: [
-             Text("Party Wear Long Cut Kurti",style: TextStyle(fontSize: 15),),
-           ]
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-             Icon(Icons.currency_rupee),
-             Text("600",style: TextStyle(fontSize: 15),),    Text("  9% off",style: TextStyle(color: const Color.fromARGB(255, 110, 108, 108)),)
-           
-           ],
-         ),
-         Row(
-          children: [
-            Text("All issue easy returns",style: TextStyle(color: const Color.fromARGB(255, 204, 202, 202)),)
-          ],
-         ),
-
-          Row(
-          children: [
-            Text("Size:32",style: TextStyle(color: const Color.fromARGB(255, 204, 202, 202)),)
-          ],
-         ),
-         SizedBox(height: 10),
-          Row(
-          children: [
-            Icon(Icons.delete),
-            Text("Remove",style: TextStyle(color: const Color.fromARGB(255, 204, 202, 202)),)
-          ],
-         ),
-            ],
-           ),
-
-          
-           
             
-          ],
-        ),
-        
-        
+                 
+                 ),
+SizedBox(height: 10,),
+                 Container(
+                  height: 100,
+                  width: double.infinity,
+                  color: const Color.fromARGB(255, 248, 246, 247),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text("Price Details (1 item)",style: TextStyle(fontSize: 15),),
+
+                      SizedBox(height: 5,),
+                      Text("Total Product Price")
+                    ],
+                  ),
+
+                    
+                                 
+                 ),
+                 Text("________________________________________________"),
+                 SizedBox(height: 20,),
+                 Row(
+                   children: [
+                     Text("Order Total"),
+                  
+                 Spacer(),
+                 Icon(Icons.currency_rupee),
+                 Text("600"),
+                   ]
+                 ),
+                 
+                SizedBox(height: 40,),
+                Container(
+                  height: 60,
+                  width: double.infinity,
+                  child: Row(
+                    children: [
+                      Icon(Icons.currency_rupee),
+                      Text("600"),
+                      Spacer(),
+                      Container(
+                        height: 30,
+                        width: 70,
+                        decoration: BoxDecoration(borderRadius: BorderRadius.circular(5)
+                        ,color: const Color.fromARGB(255, 245, 85, 138),),
+                        child: Text("Continue",style: TextStyle(color: Colors.white),),
+                      )
+                    ],
+                  ),
+                )
+         ],
+       ),
       
-      );
       
 
 
       
-        }),
+      
         
         
         
